a1a5907fa1093125ea50b19daf32f662024540df
billyjin <billyjin@tencent.com>
2025-04-08 11:00:57 +0800

    code format

--- src/base/Bench.cpp
+++ src/base/Bench.cpp
@@ -22,8 +22,7 @@
 #include "tgfx/platform/Print.h"
 
 namespace benchmark {
-static std::vector<Bench*> drawers = {
-    new ParticleBench(GraphicType::Rect)};
+static std::vector<Bench*> drawers = {new ParticleBench(GraphicType::Rect)};
 
 static std::vector<std::string> GetDrawerNames() {
   std::vector<std::string> names;
